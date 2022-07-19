4f2f8a1d3a13223bdfe75ebbe9f17bb1843945e0
Guilherme S <guinho.shuto@gmail.com>
2022-07-19 00:10:47 -0400

    adicao de comandos pra gerenciamento de pontos

--- twitch/index.js
+++ twitch/index.js
@@ -12,6 +12,11 @@ const client = new tmi.Client({
     channels: ['marcellus_v', 'guzcalp', 'guinhoshuto']
 });
 
+function tiraArroba(nome){
+    if(nome.substring(0,1)==='@') return nome.substring(1); 
+    return;
+}
+
 const handleMessages = (channel, tags, message, self) => {
     if(self) return;
     switch( message.toLowerCase()){
@@ -31,6 +36,7 @@ const handleMessages = (channel, tags, message, self) => {
             client.say(channel, `o ousado chegou`)
             break;
         case '!rachadinha':
+            axios.put(`http://feras-leaderboards.herokuapp.com/guzclap/twitch/dividaJu/${tiraArroba(tags.username)}`)
             const c = channel.substring(1)
             axios.get(`http://feras-leaderboards.herokuapp.com/find/${c}/${tags.username}`)
             .then(user => {
@@ -46,12 +52,41 @@ const handleMessages = (channel, tags, message, self) => {
             console.log(channel)
             break;
     }
+    if(tags.username === 'mazeeein'){
+        if(message === '!slots 13'){
+            client.say(channel, `@mazeeein você roletou guzcalRedstar | guzcalRedstar | guzcalRedstar`) 
+        }
+    }
+
+    if(tags.mod){
+        const words = message.split(" ");
+        let member = '';
+        let att = '';
+        switch(words[0]){
+            case '!addkappa':
+                att = 'kappa';
+                member = words[1];
+            case '!addFirst':
+                att = 'first';
+                member = words[1];
+        }
+        const guzEndpoint = `https://feras-leaderboards.herokuapp.com/guzclap/twitch/${att}/${tiraArroba(member)}`; 
+        axios.put(guzEndpoint)
+        .then(() => client.say(channel, "prontinho"))
+        .catch(e => {
+            client.say(channel, "vc não manda em mim (mentira, deu algum ruim aqui)");
+            console.log('e', e);
+        })
+    }
+
     if(message.substring(0,3) === '!ju') {
         const msgJu = message.substring(4);
         console.log(msgJu.substring(0, 11));
         if(msgJu.substring(0, 7) === '!gamble'){
             client.say(channel, `@${tags.username}, ô porra!`)
-        if(msgJu.substring(0, 11) === '!givepoints'){
+        } else if(msgJu.substring(0, 6) === '!slots'){
+            client.say(channel, `Hoje não, Faro!`)
+        } else if(msgJu.substring(0, 11) === '!givepoints'){
             client.say(channel, `algum mod pode dar ban no @${tags.username}, por favor?`)
         } else {
             client.say(channel, `${msgJu}`)
